8efca7d90ce2ef5ce11b6b4ed30ff2892de1c46a
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-02 13:10:25 +0530

    Update maximumSubarray.java
    /**
     * Problem: Maximum Subarray
     * 🔗 LeetCode Link: https://leetcode.com/problems/maximum-subarray/
     *
     * Description:
     * Given an integer array `nums`, find the contiguous subarray (containing at least one number)
     * which has the largest sum and return its sum.
     *
     * ✅ Approach:
     * This solution uses **Kadane’s Algorithm**.
     * - Initialize two variables: `maxSum` and `sum`, both set to the first element.
     * - Traverse the array from index 1:
     *     - At each step, update `sum` as the maximum of current element or `sum + current element`
     *     - Update `maxSum` if `sum' is greater
     * - Return `maxSum` as the result.
     *
     *  Time Complexity: O(n)
     *  Space Complexity: O(1)

--- Arrays/maximumSubarray.java
+++ Arrays/maximumSubarray.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int num:nums){
+            sum += num;
+            maxSum = Math.max(sum,maxSum);
+            if(sum<0) sum=0; 
+        }
+        return maxSum;
+        
+    }
+}

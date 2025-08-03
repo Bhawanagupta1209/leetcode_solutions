c7c2956bf01ef402f25bce3ae1a09d901f8c4bdb
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-03 18:37:38 +0530

    Update containerWithMostWater.java

--- Arrays/containerWithMostWater.java
+++ Arrays/containerWithMostWater.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int water = 0,maxWater = 0;
+        int left = 0,right=n-1;
+        while(left<right){
+            int min = Math.min(height[left],height[right]);
+            water = min*(right-left);
+            maxWater = Math.max(water,maxWater);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxWater;
+        
+    }
+}

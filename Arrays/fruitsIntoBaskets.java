4924f875c8cdadb3cd398b2f2a8b60247e134c5e
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-04 09:03:12 +0530

    Update fruitsIntoBaskets.java

--- Arrays/fruitsIntoBaskets.java
+++ Arrays/fruitsIntoBaskets.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int left = 0,right=0;
+        int maxFruits = 0;
+        Map<Integer,Integer> countMap = new HashMap<>();
+        for(right=0;right<n;right++){
+            countMap.put(fruits[right],countMap.getOrDefault(fruits[right],0)+1);
+            if(countMap.size()>2){
+                countMap.put(fruits[left],countMap.get(fruits[left])-1);
+                if(countMap.get(fruits[left])==0){
+                    countMap.remove(fruits[left]);
+                }
+                left++;
+            }
+            maxFruits = Math.max(maxFruits,right-left+1);
+        }
+        return maxFruits;
+        
+    }
+}

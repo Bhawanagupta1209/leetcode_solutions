473df7b27a9a88a2553410af50f7324bc8ed8581
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-04 23:54:19 +0530

    Update KthLargestElement.java

--- Arrays/KthLargestElement.java
+++ Arrays/KthLargestElement.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // Min-Heap to keep track of the k largest elements
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num:nums){
+            minHeap.add(num);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    
+    }
+}

31fc26b935ca70422d4b3f8cdea2357c16e5d06b
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-04 11:34:28 +0530

    Update findAllAnagramsInAString.java

--- Strings/findAllAnagramsInAString.java
+++ Strings/findAllAnagramsInAString.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if(s.length()<p.length()) return result;
+        Map<Character,Integer> pMap = new HashMap<>();
+        Map<Character,Integer> freqMap = new HashMap<>();
+        for(char c:p.toCharArray()){
+            pMap.put(c,pMap.getOrDefault(c,0)+1);
+        }
+        int right=0,left=0;
+        while(right<s.length()){
+            char c = s.charAt(right);
+            freqMap.put(c,freqMap.getOrDefault(c,0)+1);
+            // Shrink the window if size exceeds p length
+            if(right-left+1>p.length()){
+                char leftC = s.charAt(left);
+                freqMap.put(leftC,freqMap.get(leftC)-1);
+                if(freqMap.get(leftC)==0){
+                    freqMap.remove(leftC);
+                }
+                left++;
+            }
+            //Compare Maps
+            if(right-left+1==p.length()&&freqMap.equals(pMap)){
+                result.add(left);
+            }
+            right++;
+        }
+        return result;
+    }
+}

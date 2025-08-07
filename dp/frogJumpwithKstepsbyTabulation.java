0ea7de5470fd7200034c9ae188bb0e21fd079ef0
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-07 17:32:22 +0530

    Update frogJumpwithKstepsbyTabulation.java

--- dp/frogJumpwithKstepsbyTabulation.java
+++ dp/frogJumpwithKstepsbyTabulation.java
@@ -1,1 +1,32 @@
+import java.util.*;
+class Main {
+    public static int frogJump(int[] heights,int k) {
+        int n = heights.length;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for(int i=1;i<n;i++){
+            int minSteps = Integer.MAX_VALUE;
+            for(int j=1;j<=k;j++){
+                if(i-j >= 0){
+                    int jump = dp[i-j]+Math.abs(heights[i]-heights[i-j]);
+                    minSteps = Math.min(minSteps,jump);
+                }
+            }
+            dp[i] = minSteps;
+        }
+        return dp[n-1];
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] heights = new int[n];
+        for(int i=0;i<n;i++){
+            heights[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int[] dp = new int[n];
+        System.out.print(frogJump(heights,k)); // Output: 30
+    }
+    
+}

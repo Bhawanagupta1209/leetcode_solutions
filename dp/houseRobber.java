027b674410e744c80c5cd78d96352d8ac3e8c078
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-09 08:34:59 +0530

    Update houseRobber.java

--- dp/houseRobber.java
+++ dp/houseRobber.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i] + prev2;
+            int notTake = 0+prev;
+            int curri = Math.max(take,notTake);
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+        
+    }
+}

e674d8409501d8ae856d4f8e0b6e0624b7909f7c
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-17 18:42:41 +0530

    Update minimumFallingPathSum.java

--- dp/minimumFallingPathSum.java
+++ dp/minimumFallingPathSum.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        for(int j=0;j<n;j++){
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int up = dp[i-1][j];
+                int leftDiag = (j>0) ? dp[i-1][j-1] : Integer.MAX_VALUE;
+                int rightDiag = (j<n-1) ? dp[i-1][j+1] : Integer.MAX_VALUE;
+                dp[i][j] = matrix[i][j] + Math.min(up,Math.min(leftDiag,rightDiag));
+            }
+        }
+        int minSum = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            minSum = Math.min(minSum, dp[n - 1][j]);
+        }
+        return minSum;
+    }
+}

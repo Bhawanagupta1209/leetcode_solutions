a916277aa3ce5f0a645b3a86f51e5d9f4a787d14
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-18 17:50:47 +0530

    Update MinCostClimbingStairs.java

--- dp/MinCostClimbingStairs.java
+++ dp/MinCostClimbingStairs.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost == null || cost.length == 0) {
+            return 0;
+        }
+        int first = cost[0];
+        int second = cost[1];
+        for(int i = 2; i < cost.length; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+        return Math.min(first, second);
+    }
+}

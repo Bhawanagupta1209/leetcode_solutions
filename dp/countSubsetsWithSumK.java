eb5b49964bceb5b74689d9deeb65106bbc3c6d5b
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-08-21 15:44:49 +0530

    Update countSubsetsWithSumK.java

--- dp/countSubsetsWithSumK.java
+++ dp/countSubsetsWithSumK.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int perfectSum(int[] arr, int K) {
+        int n = arr.length;
+        int[][] dp = new int[n][K+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        if(a[0]<K) dp[0][a[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int s=0;s<K;s++){
+                int notPick = dp[i-1][s];
+                int pick = 0;
+                if(arr[index]<=K){
+                    pick = dp[i-1][s-arr[i]];
+                }
+            }
+        }
+       return f(n-1,K,arr,dp);
+    }
+}
 

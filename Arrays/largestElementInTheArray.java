f7b283cc7d9df733ffa720b394e4e5d70ac0e76b
Bhawanagupta1209 <156784728+Bhawanagupta1209@users.noreply.github.com>
2025-09-02 20:00:48 +0530

    Update largestElementInTheArray.java

--- Arrays/largestElementInTheArray.java
+++ Arrays/largestElementInTheArray.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int largestElement(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+}
